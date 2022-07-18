512ea9baa4a3f42a326efdacde6582614e269f81
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-07-18 18:12:39 +0530

    added create todo function

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,4 +1,11 @@
+import Todo from "../models/Todo.js";
+
+const createTodo = async (req, res) => {
+  const todo = await Todo.create(req.body);
+  return res.status(201).json({ todo });
+};
 const getAllTodosStatic = async (req, res) => {
+  throw new Error("testing async error");
   return res.status(200).json({ msg: "todos testing route" });
 };
 
@@ -6,4 +13,4 @@ const getAllTodos = async (req, res) => {
   return res.status(200).json({ msg: "get all todos" });
 };
 
-export { getAllTodosStatic, getAllTodos };
+export { getAllTodosStatic, getAllTodos, createTodo };
