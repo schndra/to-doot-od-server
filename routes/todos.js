b0485d72669393d5135fe9da45a6dfe13c877c2c
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-07-18 18:13:47 +0530

    create todo route

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,13 @@
 import express from "express";
 const router = express.Router();
 
-import { getAllTodos, getAllTodosStatic } from "../controllers/todos.js";
+import {
+  getAllTodos,
+  getAllTodosStatic,
+  createTodo,
+} from "../controllers/todos.js";
 
-router.route("/").get(getAllTodos);
+router.route("/").get(getAllTodos).post(createTodo);
 router.route("/static").get(getAllTodosStatic);
 
 export default router;
