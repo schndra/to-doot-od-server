e9cc3f4ec8444b2f46d022a02c45e2a6c6e98a76
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-07-25 17:58:35 +0530

    completed creating a todo and getting all todos

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,15 +2,17 @@ import Todo from "../models/Todo.js";
 
 const createTodo = async (req, res) => {
   const todo = await Todo.create(req.body);
-  return res.status(201).json({ todo });
+  return res.status(201).json({ todo, msg: "successfully create todo" });
 };
 const getAllTodosStatic = async (req, res) => {
-  throw new Error("testing async error");
-  return res.status(200).json({ msg: "todos testing route" });
+  // throw new Error("testing async error");
+  const todo = await Todo.find({});
+  return res.status(200).json({ todo });
 };
 
 const getAllTodos = async (req, res) => {
-  return res.status(200).json({ msg: "get all todos" });
+  const todo = await Todo.find({});
+  return res.status(200).json({ todo });
 };
 
 export { getAllTodosStatic, getAllTodos, createTodo };
